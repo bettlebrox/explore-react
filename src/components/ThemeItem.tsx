a876e525bd0da1668e1882d100a0f0934ce31cef
David Burke <474346+bettlebrox@users.noreply.github.com>
2024-07-11 18:03:31 +0100

    chore: style delete button

--- src/components/ThemeItem.tsx
+++ src/components/ThemeItem.tsx
@@ -1,36 +1,29 @@
 import { Theme } from "../interfaces/Theme";
-import {
-  Card,
-  CardActionArea,
-  CardActions,
-  CardHeader,
-  IconButton,
-  Skeleton,
-  Typography,
-} from "@mui/material";
+import { Card, CardActionArea, CardActions, CardHeader, IconButton, Skeleton, Typography } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getFormattedDate } from "../utils/format";
 import { iconThemeTypeMap } from "../utils/map";
 import { Link } from "react-router-dom";
 import { del } from "aws-amplify/api";
 import { useMutation, useQueryClient } from "react-query";
+import { faClose, faDeleteLeft, faTrash } from "@fortawesome/free-solid-svg-icons";
 
-async function delTheme(title:string){
+async function delTheme(title: string) {
   try {
     const restOperation = del({
       apiName: "Dassie",
-      path: "/api/themes/" +title,
+      path: "/api/themes/" + title,
     });
     await restOperation.response;
     console.log("DELETE call succeeded");
-  }catch (error) {
+  } catch (error) {
     console.log("DELETE call failed: ");
   }
 }
 export function ThemeItem({
   theme,
   expanded,
-  isPlaceholderData: isPlaceholderData
+  isPlaceholderData: isPlaceholderData,
 }: {
   theme: Theme;
   expanded?: boolean;
@@ -44,34 +37,38 @@ export function ThemeItem({
     },
     onMutate: () => {
       queryClient.cancelQueries(["customThemesData"]);
-    }
+    },
   });
-  const actions = <CardActions><IconButton onClick={() => deleteTheme.mutate(theme.title)}>Delete</IconButton></CardActions>;
+  const actions = (
+    <CardActions>
+      <IconButton onClick={() => deleteTheme.mutate(theme.title)} size="small">
+        <FontAwesomeIcon icon={faTrash} />
+      </IconButton>
+    </CardActions>
+  );
   let subheader = <></>;
   let avatar: JSX.Element | null = null;
   if (expanded) {
-    avatar =
-      isPlaceholderData ? (
-        <IconButton>
-          <Skeleton animation="wave" variant="circular" width={32} height={32} />
-        </IconButton>
-      ) : (
-        <IconButton>
-          <FontAwesomeIcon icon={iconThemeTypeMap[theme.source]} />
-        </IconButton>
-      );
-    subheader = (
-      isPlaceholderData ? (
-        <>
-          <Typography variant="body2" align="left">
-            <Skeleton animation="wave" variant="text" width={200} />
-          </Typography>
-          <Typography variant="body2" align="left">
-            <Skeleton animation="wave" variant="text" width={200} />
-          </Typography>
-        </>
-      ):(
-        <>
+    avatar = isPlaceholderData ? (
+      <IconButton>
+        <Skeleton animation="wave" variant="circular" width={32} height={32} />
+      </IconButton>
+    ) : (
+      <IconButton>
+        <FontAwesomeIcon icon={iconThemeTypeMap[theme.source]} />
+      </IconButton>
+    );
+    subheader = isPlaceholderData ? (
+      <>
+        <Typography variant="body2" align="left">
+          <Skeleton animation="wave" variant="text" width={200} />
+        </Typography>
+        <Typography variant="body2" align="left">
+          <Skeleton animation="wave" variant="text" width={200} />
+        </Typography>
+      </>
+    ) : (
+      <>
         <Typography variant="body2" align="left">
           {getFormattedDate(theme.created_at)}
         </Typography>
@@ -79,20 +76,25 @@ export function ThemeItem({
           {theme.summary}
         </Typography>
       </>
-      )
     );
   }
   return (
-    <Card className="theme-item" variant="outlined" sx={{ minWidth: 520 }}>
+    <Card className="theme-item" variant="outlined" sx={{ minWidth: 520, display: "flex" }}>
       <CardActionArea component={Link} to={"/theme/" + theme.title}>
         <CardHeader
-          title={isPlaceholderData?(<Skeleton animation="wave" variant="text" width={200} />):(<Typography>{theme.original_title}</Typography>)}
+          title={
+            isPlaceholderData ? (
+              <Skeleton animation="wave" variant="text" width={200} />
+            ) : (
+              <Typography>{theme.original_title}</Typography>
+            )
+          }
           align="left"
           subheader={subheader}
           avatar={avatar}
         />
       </CardActionArea>
-        {actions}
+      {actions}
     </Card>
   );
 }
