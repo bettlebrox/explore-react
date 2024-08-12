d07ff0ce4464048954a7ade56eee7eb35983c2fe
David Burke <474346+bettlebrox@users.noreply.github.com>
2024-08-12 09:16:17 +0100

    fix: add permission to invoke API

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,19 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+//allow any authorised user to access the api
+backend.auth.resources.authenticatedUserIamRole.addToPrincipalPolicy(
+  new PolicyStatement({
+    actions: ['execute-api:Invoke'],
+    resources: ['arn:aws:execute-api:eu-west-1:559845934392:p5cgnlejzk/prod/*/*/*'],
+  }),
+);
